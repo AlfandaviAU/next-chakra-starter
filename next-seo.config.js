c8dbb7a8471f172b33df038d597053f20e11ab4b
Muhammad Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2024-03-28 13:08:44 +0700

    fix: seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,32 +1,23 @@
-const title = 'Instagram';
+const title = 'BOKEP TERBARU 2024!';
 const description =
-  'An opinionated starter project with Next.js and Chakra UI. Created by hendraaagil';
-const url = 'https://nextjs-chakra-hendraaagil.vercel.app';
+  'Pilu Siswi SMA di Lampung Disekap dan Diperkosa Teman Pria FULL VIDEO!';
 
 const SEO = {
   title,
   description,
-  canonical: url,
   openGraph: {
     type: 'website',
-    url,
     title,
     description,
     images: [
       {
-        url: `https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png`,
+        url: `https://images4.imagebam.com/7e/e7/a8/MESPCCB_o.png`,
         alt: title,
         width: 2048,
         height: 1170,
       },
     ],
   },
-  twitter: {
-    cardType: 'summary_large_image',
-    handle: '@hendraaagil',
-    site: '@hendraaagil',
-  },
-  additionalLinkTags: [{ rel: 'icon', href: '/favicon.ico' }],
 };
 
 export default SEO;
